81eb606b57ee7938bd84bd604d813cd82295b346
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-10-12 22:04:26 -0400

    add implementação do chat, sendo enviar e receber msn, sala e nome user. Backend e Frontend.

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const socket = require('socket.io');
 const cors = require('cors')
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
@@ -23,4 +25,20 @@ io = socket(server, {cors: {origin: "*"}});
 
 io.on("connection", (socket) => {
   console.log(socket.id);
+
+  // Recebe a seleção de sala do front-end, usando o metodo On
+  socket.on("sala_conectar", (dados) => {
+    console.log("Sala selecionada: " + dados);
+    socket.join(dados);
+  });
+
+  // Recebe a Mensagem do front-end, usando o metodo On
+  socket.on("enviar_mensagem", (dados) => {
+    console.log(dados);
+    
+    // Enviar a mensagem para todos os usuarios da sala por broadcasting, que estão no front-end
+    socket.to(dados.sala).emit("receber_mensagem", dados.conteudo);
+
+  });
+
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+let socket;
 
 function App() {
+
+  const ENDPOINT = "http://localhost:8000/";
+
+  const [logado, setLogado] = useState(false);
+  const [nome, setNome] = useState(" ");
+  const [sala, setSala] = useState(" ");
+
+  const [mensagem, setMensagem] = useState(" "); // Para enviar mensagem
+  const [listaMensagem, setListaMensagem] = useState([]); // Para receber mensagem
+
+
+  useEffect(() => {
+    socket = socketIOClient(ENDPOINT);
+  },[]);
+
+  useEffect(() => {
+    socket.on("receber_mensagem", (dados) => {
+      setListaMensagem([...listaMensagem, dados]);
+    });
+  });
+
+ // Envia a sala para o Back-end
+  const conectarSala = () => {
+    console.log("Acessou a sala " + sala + " com o nome " + nome);
+    setLogado(true);
+    // Metodo para enviar para o Back-end 
+    socket.emit("sala_conectar", sala);
+  }
+
+ // Envia a Mensagem para o Back-end
+  const enviarMensagem = async () => {
+    console.log("Mensagem: " + mensagem);
+    const conteudoMensagem = {
+      sala,
+      conteudo: {
+        nome,
+        mensagem
+      }
+    }
+    console.log(conteudoMensagem);
+
+    // Metodo para enviar para o Back-end 
+    await socket.emit("enviar_mensagem", conteudoMensagem);
+    // Para receber broadcasting com as mensagens da sala
+    setListaMensagem([ ...listaMensagem, conteudoMensagem.conteudo]);
+    setMensagem("");
+  }
+
   return (
     <div>
       <h1>Chat</h1>
+      {!logado ? 
+      <>
+        <label>Nome: </label>
+        <input type="text" placeholder="Nome" name="nome" value={nome} onChange={(texto) => {setNome(texto.target.value)}
+        } /> <br /><br />
+        <label>Sala: </label>
+        {/* <input type="text" placeholder="Sala" value={sala} onChange={(text) => {setSala(text.target.value)}
+        } /> <br /><br /> */}
+        <select name="sala" value={sala} onChange={texto => setSala(texto.target.value)}>
+          <option value="">Selecione</option>
+          <option value="1">Node.js</option>
+          <option value="2">React.js</option>
+          <option value="3">React Native</option>
+          <option value="4">PHP</option>
+        </select><br /><br />
+        <button onClick={conectarSala}>Acessar</button>
+      </>
+      : 
+      <>
+      {/* Envia as mensagens para o Back-end*/}
+      {listaMensagem.map((msg, key) => {
+        return (
+          <div key={key}>
+            {msg.nome}: {msg.mensagem}
+          </div>
+        )
+      })}
+        <input type="text" name="mensagem" value={mensagem} placeholder="Mensagem..." onChange={(texto) => {setMensagem(texto.target.value)}} />
+        <button onClick={enviarMensagem}>Enviar</button>
+      </>
+      }
     </div>
   );
 }
